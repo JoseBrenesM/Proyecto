9d586636821dba19699173db3dbb6d7c819de6f5
Flayeer69 <95499394+Flayeer69@users.noreply.github.com>
2021-12-03 13:49:20 -0600

    Se creo la clase de datos para realizar compra.

---
+++ src/avance/iv/datos.java
@@ -0,0 +1,16 @@
+package proyecto;
+
+public class datos
+{
+    public String nombre;
+    public String apellidos;
+    public String mail;
+    public String telefono;
+    public datos(String nombre, String apellidos, String mail, String telefono)
+    {
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.mail=mail;
+        this.telefono=telefono;
+    }
+}
